84ec76cbf6a774678745370763adc2a808edbfec
weibaozi <guo361270936@gmail.com>
2024-08-15 21:35:11 -0400

    first

---
+++ lorahub/algorithm3_grad_old.py
@@ -0,0 +1,468 @@
+from transformers import AutoModelForSeq2SeqLM
+import torch
+from datasets import Dataset
+from torch.utils.data import DataLoader
+from transformers import default_data_collator
+from transformers import AutoTokenizer
+from tqdm import tqdm
+import pandas as pd
+import numpy
+import random
+import nevergrad as ng
+from peft.utils.save_and_load import set_peft_model_state_dict, get_peft_model_state_dict
+from peft import PeftModel, PeftConfig
+from functools import partial
+from typing import List, Optional, Union
+import copy
+from torch.autograd import Variable
+import torch.optim as optim
+
+from torchviz import make_dot
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+def load_base_model_and_lora_modules(lora_module_list: List[str], model_name_or_path: Optional[str] = None):
+    """load base model and lora modules from huggingface model hub
+
+    Args:
+        lora_module_list (List[str]): a list of lora module names available in huggingface model hub
+        model_name_or_path (Optional[str]): base model name, default is None
+    """
+    # use gpu if available
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # load basic model
+    default_peft_model_id = lora_module_list[0]
+    # find the base model
+    if model_name_or_path is None:
+        model_name_or_path = PeftConfig.from_pretrained(default_peft_model_id).base_model_name_or_path
+        
+    base_model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
+    # load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    # 0 is the default model
+    try:
+        peft_model = PeftModel.from_pretrained(base_model, default_peft_model_id)
+    except:
+        raise Exception(f'{default_peft_model_id} is unable to load into the model {model_name_or_path}')
+        
+    peft_model = peft_model.to(device)
+    peft_model.eval()
+
+    print("> Begin to load lora modules")
+    cache = {}
+
+    first_dict = None
+
+    for peft_model_id in tqdm(lora_module_list):
+        print("> Loading {} ...".format(peft_model_id))
+        cur_peft_model = PeftModel.from_pretrained(base_model, peft_model_id)
+        cache[peft_model_id] = copy.deepcopy(get_peft_model_state_dict(cur_peft_model))
+
+        if first_dict is None:
+            first_dict = cache[peft_model_id]
+        # check whether the LoRA can be merged into one 
+        try:
+            # detect whether the arch is the same
+            for key in first_dict.keys():
+                assert first_dict[key].shape == cache[peft_model_id][key].shape
+        except:
+            raise Exception(f'LoRA Modules {peft_model_id} cannot be merged since it has a different arch (e.g., rank).')
+               
+    return peft_model, tokenizer, cache
+
+def preprocess_function(examples, tokenizer):
+    """
+    standard preprocess function for dataset
+    """
+    inputs = examples["input"]
+    targets = examples["output"]
+    model_inputs = tokenizer(
+        inputs,
+        max_length=2048,
+        padding=True,
+        truncation=True,
+        return_tensors="pt",
+    )
+    labels = tokenizer(
+        targets,
+        max_length=2048,
+        padding=True,
+        truncation=True,
+        return_tensors="pt",
+    )
+    labels = labels["input_ids"]
+    labels[labels == tokenizer.pad_token_id] = -100
+    model_inputs["labels"] = labels
+    return model_inputs
+
+
+def load_dataset(example_inputs, example_outputs, tokenizer):
+    # add empty string if example_outputs is None
+    if example_outputs is None:
+        example_outputs = [""] * len(example_inputs)
+    df = [
+        {"input": example_inputs[i], "output": example_outputs[i]}
+        for i in range(len(example_inputs))
+    ]
+    dataset = Dataset.from_pandas(pd.DataFrame(df))
+    preprocess_func_with_tokenizer = partial(preprocess_function, tokenizer=tokenizer)
+    processed_datasets = dataset.map(
+        preprocess_func_with_tokenizer,
+        batched=True,
+        num_proc=1,
+        desc="Running tokenizer on dataset",
+    )
+    return processed_datasets
+
+
+def default_get_loss(example_dataset, model, batch_size, compute_gradients=True):
+    """
+    Get the loss of the model on the example dataset.
+    If compute_gradients is True, computes the loss with gradients (for training).
+    Otherwise, computes the loss without gradients (for evaluation).
+    """
+    data_batch_size = len(example_dataset) if batch_size is None else min(len(example_dataset), batch_size)
+    
+    # Create a DataLoader to batch the example dataset
+    train_dataloader = DataLoader(
+        example_dataset,
+        collate_fn=default_data_collator,
+        batch_size=data_batch_size,
+        pin_memory=True,  # If True, the data loader will copy tensors into CUDA pinned memory before returning them
+    )
+    
+    train_loss = 0
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    for _, batch in enumerate(train_dataloader):
+        batch = {k: v.to(device) for k, v in batch.items()}  # Move batch to the appropriate device
+        
+        if compute_gradients:
+            # Compute loss with gradients (training)
+            outputs = model(**batch)  # Forward pass through the model
+            loss = outputs.loss
+            print("loss:",loss.item())
+            train_loss += loss  # Accumulate the loss
+        else:
+            # Compute loss without gradients (evaluation)
+            with torch.no_grad():
+                outputs = model(**batch)  # Forward pass through the model
+                loss = outputs.loss
+                train_loss += loss.detach().float()  # Accumulate the loss
+    
+    # Return the average loss over the number of examples
+    if compute_gradients:
+        return train_loss / len(example_dataset["input"])  # Keep as tensor for backpropagation
+    else:
+        return float(train_loss) / len(example_dataset["input"]) 
+    # average loss over the number of examples
+    return float(loss) / len(example_dataset["input"])
+
+def default_l1_regularization(weights):
+    """
+    Get the L1 regularization term for the weights
+    """
+    sum_of_squares = sum([abs(x) for x in weights.flatten()]) / len(weights.flatten())
+    return 0.0005 * sum_of_squares
+
+def get_score(weights, model, cache, example_dataset, batch_size, get_loss, get_regular):
+    # # the composed lora state dict
+    # final_state_dict = {}
+    # # module list is the list
+    # lora_module_list = list(cache.keys())
+    # # print("length of lor modules:",len(lora_module_list))
+    # # print(weights)
+    # # all keys are the same
+
+    # keys = cache[lora_module_list[0]].keys()
+    # # print(len(keys))
+    # # print("keys:",keys)
+    # # raise Exception("stop")
+    # for i, peft_model_id in enumerate(lora_module_list):
+    #     lora_state_dict = cache[peft_model_id]
+    #     if i == 0:
+    #         for j,key in enumerate(keys):
+    #             if 'encoder' in key:
+    #                 final_state_dict[key] = weights[i][j//4] * lora_state_dict[key]
+    #             else:
+    #                 final_state_dict[key] = weights[i][j//8+12] * lora_state_dict[key]
+    #     else:
+    #         for j,key in enumerate(keys):
+    #             if 'encoder' in key:
+    #                 final_state_dict[key] = (
+    #                     final_state_dict[key] + weights[i][j//4] * lora_state_dict[key]
+    #                 )
+    #             else:
+    #                 final_state_dict[key] = (
+    #                     final_state_dict[key] + weights[i][j//8+12] * lora_state_dict[key]
+    #                 )
+    # # reload the model with the new adapter config
+    # # set_peft_model_state_dict(model, final_state_dict)
+    # for name, param in model.named_parameters():
+    #     if name in final_state_dict:
+    #         param.requires_grad = True
+    #         param.copy_(final_state_dict[name])
+
+    # for k,v in final_state_dict.items():
+    #     if v.requires_grad:
+    #         print(f"{k} requires gradient.")
+    #     else:
+    #         print(f"{k} does not require gradient.")
+    # for name, param in model.named_parameters():
+    #     if param.requires_grad:
+    #         print(f"{name} requires gradient.")
+    #     else:
+    #         print(f"{name} does not require gradient.")
+        
+    # minimize the metric
+    loss = get_loss(example_dataset, model, batch_size)
+    # L1 regularization term
+    metric_val = loss + get_regular(weights)
+
+    # metric_val = loss
+    
+    
+    return metric_val
+
+def get_final_weights(weights, lora_module_list, cache):
+    final_state_dict = {}
+    keys = cache[lora_module_list[0]].keys()
+    for i, peft_model_id in enumerate(lora_module_list):
+        lora_state_dict = cache[peft_model_id]
+        if i == 0:
+            for j,key in enumerate(keys):
+                if 'encoder' in key:
+                    final_state_dict[key] = weights[i][j//4] * lora_state_dict[key]
+                else:
+                    final_state_dict[key] = weights[i][j//8+12] * lora_state_dict[key]
+        else:
+            for j,key in enumerate(keys):
+                if 'encoder' in key:
+                    final_state_dict[key] = (
+                        final_state_dict[key] + weights[i][j//4] * lora_state_dict[key]
+                    )
+                else:
+                    final_state_dict[key] = (
+                        final_state_dict[key] + weights[i][j//8+12] * lora_state_dict[key]
+                    )
+
+    return final_state_dict
+    
+def lorahub_inference(example_inputs: List[str],
+                      model_or_name_path: Union[AutoModelForSeq2SeqLM, str],
+                      tokenizer_or_tokenizer_path: Union[AutoTokenizer, str],
+                      batch_size: int,
+                      # if not provided, we do not report the accuracy
+                      example_outputs: List[str]=None):
+    
+    def accuracy_score(outputs, ground_truths):
+        correct = 0
+        total = 0
+        for output, truth in zip(outputs, ground_truths):
+            if output.strip().lower().replace(".", "") == truth.strip().lower().replace(".", ""):
+                correct += 1
+            total += 1
+        return correct / total * 100
+
+    example_predictions = []
+    # load model
+    if isinstance(model_or_name_path, str):
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_or_name_path)
+    else:
+        model = model_or_name_path
+    
+    # load tokenizer
+    if isinstance(tokenizer_or_tokenizer_path, str):
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_or_tokenizer_path)
+    else:
+        tokenizer = tokenizer_or_tokenizer_path
+            
+    # for name, param in model.named_parameters():
+    #     if "lora" not in name:
+    #         param.requires_grad = False
+    # process dataset
+    dataset = load_dataset(example_inputs, example_outputs, tokenizer)
+    # use gpu if available
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = model.to(device)
+
+    for i in range(0, len(dataset["input"]), batch_size):
+        inputs = tokenizer(
+            dataset["input"][i : i + batch_size],
+            max_length=2048,
+            return_tensors="pt",
+            padding=True,
+        ).to(device)
+        outputs = model.generate(
+            input_ids=inputs["input_ids"], max_new_tokens=256
+        )
+        outputs = tokenizer.batch_decode(
+            outputs.to("cpu"), skip_special_tokens=True
+        )
+        example_predictions.extend(outputs)
+    
+    if example_outputs is not None:
+        task_perf = accuracy_score(example_predictions, example_outputs)
+    else:
+        task_perf = None
+        
+    return example_predictions, task_perf
+
+def lorahub_learning(lora_module_list: List[str], 
+                     example_inputs: List[str], 
+                     example_outputs: List[str], 
+                     max_inference_step: int,
+                     model_name_or_path=None,
+                     batch_size=None,
+                     get_loss=default_get_loss, 
+                     get_regular=default_l1_regularization,
+                     seed=42,
+                     early_stopping=True,
+                     lr=0.01):
+    # set seed for reproducibility
+    random.seed(seed)
+    numpy.random.seed(seed)
+
+    number_of_loras = len(lora_module_list)
+    if number_of_loras == 0:
+        print("> No LoRA modules are provided. Please provide at least one LoRA module.")
+        return None, None
+
+    # load model
+    model, tokenizer, cache = load_base_model_and_lora_modules(lora_module_list, model_name_or_path)
+    # for name, param in model.named_parameters():
+    #     if param.requires_grad:
+    #         print(f"{name} requires gradient.")
+    #     else:
+    #         print(f"{name} does not require gradient.")
+    # process dataset
+    dataset = load_dataset(example_inputs, example_outputs, tokenizer) 
+    
+    # set up the limit of the weights dimension 24 * number_of_loras
+    num_blocks=len(cache[lora_module_list[0]].keys())//6
+    print("num_blocks:",num_blocks)
+    # standard
+    # params = Variable(torch.rand(number_of_loras, num_blocks ),requires_grad=True)
+    # xavier]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # params_ = Variable(torch.empty(number_of_loras, num_blocks), requires_grad=True)
+    params = torch.empty(number_of_loras, num_blocks, device=device, requires_grad=True)
+    torch.nn.init.xavier_uniform_(params)
+    loss = (params ** 2).sum()
+
+    # Perform a backward pass to compute gradients
+    loss.backward()
+    # for i in range(params.shape[0]):
+    #     for j in range(params.shape[1]):
+    #         # if params[i][j].grad is None:
+    #             params.grad[i,j] = torch.zeros_like(params[i][j])
+    #             print(params.grad[i,j])
+
+    optimizer = optim.Adam([params], lr=lr) #weight decay
+    
+    final_state_dict = {}
+    keys = cache[lora_module_list[0]].keys()
+    # for name, param in model.named_parameters():
+    #     #extract string .default from the name
+    #     name_processed = name.replace(".default","")
+    #     if name_processed in keys:
+    #         # print(f"{name} requires gradient.")
+            
+    #         param.requires_grad = True
+    key_params_lookup = {}
+    def update_lora():
+        for i, peft_model_id in enumerate(lora_module_list):
+            lora_state_dict = cache[peft_model_id]
+            if i == 0:
+                for j,key in enumerate(keys):
+                    if 'encoder' in key:
+                        final_state_dict[key] = params[i][j//4] * lora_state_dict[key]
+                        key_params_lookup[key] = [(i,j//4,peft_model_id)]
+                    
+                    else:
+                        final_state_dict[key] = params[i][j//8+12] * lora_state_dict[key]
+                        key_params_lookup[key] = [(i,j//8+12,peft_model_id)]
+            else:
+                for j,key in enumerate(keys):
+                    if 'encoder' in key:
+                        final_state_dict[key] = (
+                            final_state_dict[key] + params[i][j//4] * lora_state_dict[key]
+                        )
+                        key_params_lookup[key].append((i,j//4,peft_model_id))
+                    else:
+                        final_state_dict[key] = (
+                            final_state_dict[key] + params[i][j//8+12] * lora_state_dict[key]
+                        )
+                        key_params_lookup[key].append((i,j//8+12,peft_model_id))
+    
+        for name, param in model.named_parameters():
+            #extract string .default from the name
+            name_processed = name.replace(".default","")
+            if name_processed in final_state_dict:
+                # print(f"{name} requires gradient.")
+                
+                param.data.copy_(final_state_dict[name_processed])
+                param.requires_grad = True
+                param.grad = None
+
+    update_lora()
+    get_score_partial = partial(get_score, 
+                                model=model, 
+                                cache=cache,
+                                example_dataset=dataset,
+                                batch_size=batch_size,
+                                get_loss=get_loss, 
+                                get_regular=get_regular)
+    print("> Begin to perform gradient optimization ...")
+    patience = 10
+    best_loss = float("inf")
+    best_params = None
+    patience_counter = 0
+    for step in range(max_inference_step):
+        optimizer.zero_grad()
+        loss = get_score_partial(params)
+        #print gradient of  base_model.model.encoder.block.0.layer.0.SelfAttention.q.lora_A.default.weight
+        loss.backward()
+        for name, param in model.named_parameters():
+            name_processed = name.replace(".default","")
+            if name_processed in final_state_dict:
+                # final_state_dict[name_processed]
+                # print(param.grad.shape)
+                # print(final_state_dict[name_processed].grad.shape)
+                for i,j,peft_model_id in key_params_lookup[name_processed]:
+                    # print(param.grad,cache[peft_model_id][name_processed])
+                    params.grad[i,j] += (param.grad * cache[peft_model_id][name_processed]).sum()
+                    # result=(param.grad * cache[peft_model_id][name_processed]).sum()
+                    # print(params[i][j].grad)
+        # print(params[0][0])
+        optimizer.step()
+        # print(params[0][0])
+        update_lora()
+
+        if step % 1 == 0:
+            print(f"Step {step}, loss {loss.item()}")
+        if early_stopping:
+            if loss < best_loss:
+                best_loss = loss
+                best_params = params.detach().clone()
+                patience_counter = 0
+            else:
+                patience_counter += 1
+                if patience_counter >= patience:
+                    print(f"Early stopping at step {step}")
+                    
+                    break
+        else:
+            best_params = params.detach().clone()
+    
+    optimized_weights = best_params.cpu().numpy()
+    #save value to txt
+    with open('recommendation.txt', 'w') as f:
+        for item in optimized_weights:
+            f.write("%s\n" % item)
+    final_lora = get_final_weights(optimized_weights, lora_module_list, cache)
+    # set the final weights
+    set_peft_model_state_dict(model, final_lora)
+    model = model.merge_and_unload()
+    # print("test")
+    return optimized_weights, model, tokenizer
---
+++ reproduce_bbh.py
@@ -0,0 +1,149 @@
+from lorahub.algorithm import lorahub_inference
+import os
+import json
+from lorahub.algorithm3_grad import lorahub_learning, lorahub_inference
+from lorahub.constant import LORA_MODULE_NAMES
+import random
+from random import shuffle
+import sys
+import pandas as pd
+def evaluate_flan_results_zero_shot(folder, flan_model_name):
+    sub_dirs = os.listdir(folder)
+    result={}
+    for sub_dir in sub_dirs:
+        test_file_path = os.path.join(folder, sub_dir, "zero_shot.jsonl")
+        task_inputs, task_outputs = [], []
+        for line in open(test_file_path, "r", encoding="utf-8"):
+            example = json.loads(line)
+            task_inputs.append(example["context"])
+            task_outputs.append(example["completion"])
+        print("Evaluating on task (zero shot): ", sub_dir)
+        _,task_acc=lorahub_inference(task_inputs,
+                          flan_model_name,
+                          flan_model_name,
+                          16,
+                          task_outputs)
+        print("task accuracy:",task_acc)
+        result[sub_dir]=task_acc
+    result_pd=pd.DataFrame({'Zero-shot acc':result})
+    return result,result_pd
+        # break
+
+
+def evaluate_flan_results_few_shot(folder, flan_model_name):
+    sub_dirs = os.listdir(folder)
+    result={}
+    for sub_dir in sub_dirs:
+        test_file_path = os.path.join(folder, sub_dir, "few_shot.jsonl")
+        task_inputs, task_outputs = [], []
+        for line in open(test_file_path, "r", encoding="utf-8"):
+            example = json.loads(line)
+            task_inputs.append(example["context"])
+            task_outputs.append(example["completion"])
+        print("Evaluating on task (five shot): ", sub_dir)
+        _,task_acc=lorahub_inference(task_inputs,
+                          flan_model_name,
+                          flan_model_name,
+                          16,
+                          task_outputs)
+        result[sub_dir]=task_acc
+        print("task accuracy:",task_acc)
+        # break
+    result_pd=pd.DataFrame({'Few-shot acc':result})
+    return result,result_pd
+
+7
+def evaluate_lorahub_results_few_shot(folder, flan_model_name,save_path="results"):
+    sub_dirs = os.listdir(folder)
+    result={'lorahub avg acc':{},'lorahub max acc':{}}
+    # 5 seeds used in our experiments
+    for sub_dir in sub_dirs:
+        try:
+            # if "boolean_expression" in sub_dir:
+            #     continue
+            print("Evaluating on task (lorahub): ", sub_dir)
+            # construct the few-shot examples for lorahub learning
+            example_inputs, examples_outputs = [], []
+            example_file_path = os.path.join(folder, sub_dir, "example.jsonl")
+            for line in open(example_file_path, "r", encoding="utf-8"):
+                example = json.loads(line)
+                example_inputs.append(example["context"])
+                examples_outputs.append(example["completion"])
+                
+            # random select 5 examples for each task
+            random.seed(42)
+            shuffled_set = list(zip(example_inputs, examples_outputs))
+            random.shuffle(shuffled_set)
+            example_inputs, examples_outputs = zip(*shuffled_set)
+            # take the first 5 examples
+            example_num=100
+            example_inputs, examples_outputs = example_inputs[:example_num], examples_outputs[:example_num]
+
+            # load the zero-shot examples for evaluation
+            test_file_path = os.path.join(folder, sub_dir, "zero_shot.jsonl")
+            task_inputs, task_outputs = [], []
+            for line in open(test_file_path, "r", encoding="utf-8"):
+                example = json.loads(line)
+                task_inputs.append(example["context"])
+                task_outputs.append(example["completion"])
+
+            task_perf_list = []
+            step_result={}
+            for step in range(30,31,1):
+                for lora_num in range(100,101,1):
+                    for seed in range(1,4):
+                        random.seed(seed)
+                        print("Evaluating on task (lorahub): ", sub_dir, "with seed:", seed)
+        
+                        def get_lora_module_list(lora_num=40):
+                            return random.sample(LORA_MODULE_NAMES, lora_num) #what 
+                        # get a list of modules to be used in the composition
+                        modules = get_lora_module_list(lora_num)
+
+                        # perform LoRAHub learning
+                        module_weights, model, tokenizer = lorahub_learning(lora_module_list=modules,
+                                                                            example_inputs=example_inputs,
+                                                                            example_outputs=examples_outputs,
+                                                                            max_inference_step=step,
+                                                                            batch_size=5,lr=0.02)
+
+                        # print("module_weights:", module_weights)
+
+                        """
+                        Perform inference to get predictions
+                        """
+                        _, task_acc = lorahub_inference(example_inputs=task_inputs,
+                                                        model_or_name_path=model,
+                                                        tokenizer_or_tokenizer_path=tokenizer,
+                                                        batch_size=10,
+                                                        # can set as None if you do not have the ground truth
+                                                        example_outputs=task_outputs)
+                        print(f"task{sub_dir},seed{seed},step{step},lora_num{lora_num},acc:{task_acc}")
+                        task_perf_list.append(task_acc)
+                        # break
+                    avg_perf, max_perf = sum(task_perf_list) / len(task_perf_list), max(task_perf_list)
+                    print("average perf:", avg_perf, "best perf:", max_perf)
+                    result["lorahub avg acc"][sub_dir]=avg_perf
+                    result["lorahub max acc"][sub_dir]=max_perf
+                    save_name=f"step{step}_train{example_num}_lora_num{lora_num}_uni_l1.csv"
+                    tmp_result=pd.DataFrame(result)
+                    tmp_result.to_csv(os.path.join("results", save_name))
+                    step_result[step]=(avg_perf,max_perf)
+                
+            # print("step_result:",step_result)
+            # break
+        except Exception as e:
+            print("error:",e)
+            continue
+    result_pd=pd.DataFrame(result)
+    return result,result_pd
+if __name__ == "__main__":
+    result_folder = "results"
+    # zero_result,zero_result_df=evaluate_flan_results_zero_shot("data_bbh", "google/flan-t5-large")
+    # zero_result_df.to_csv(os.path.join(result_folder, "zero_result.csv"))
+    # # five shot for flan models
+    # few_result,few_result_df=evaluate_flan_results_few_shot("data_bbh", "google/flan-t5-large")
+    # few_result_df.to_csv(os.path.join(result_folder, "few_result.csv"))
+    # five shot for lorahub models
+    lorahub_result,lorahub_result_df=evaluate_lorahub_results_few_shot("data_bbh", "google/flan-t5-large")
+    lorahub_result_df.to_csv(os.path.join(result_folder, "lorahub_result_weight_100t_100lora_uni_l1.csv"))
